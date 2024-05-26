281cda94ec52c904250ed4746b63d6df5cbaf237
Illia Teliuk <illiateliuk@gmail.com>
2024-05-26 17:53:10 +0300

    Removed unneccessary usings

--- Task3/apz-pzpi-21-2-teliuk-illia-task3/Heisenbrew-iot/Heisenbrew-iot/Program.cs
+++ Task3/apz-pzpi-21-2-teliuk-illia-task3/Heisenbrew-iot/Heisenbrew-iot/Program.cs
@@ -1,16 +1,8 @@
 using Heisenbrew_iot;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text.Json;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Configuration;
+
 class Program
 {
     static int GetAvailablePort()
