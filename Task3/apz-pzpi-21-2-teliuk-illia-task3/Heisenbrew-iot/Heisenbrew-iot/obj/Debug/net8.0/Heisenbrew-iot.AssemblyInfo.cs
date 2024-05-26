dfab4ea5138672941ff19fd431f707a12b48f5c5
Illia Teliuk <illiateliuk@gmail.com>
2024-05-26 17:48:58 +0300

    Removed appsettings

--- Task3/apz-pzpi-21-2-teliuk-illia-task3/Heisenbrew-iot/Heisenbrew-iot/obj/Debug/net8.0/Heisenbrew-iot.AssemblyInfo.cs
+++ Task3/apz-pzpi-21-2-teliuk-illia-task3/Heisenbrew-iot/Heisenbrew-iot/obj/Debug/net8.0/Heisenbrew-iot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Heisenbrew-iot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc3f2add88f98b1f0cf4cf83635814ce8017a786")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc0b9805f25e68692c3d7b586e2387686c885c89")]
 [assembly: System.Reflection.AssemblyProductAttribute("Heisenbrew-iot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Heisenbrew-iot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
